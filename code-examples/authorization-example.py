4b1fbfa0de2c3c3d1b8ce4d59a0439454cc3563d
ssiadak <steve.siadak@netscout.com>
2022-11-11 12:20:58 -0500

    update authorization-example.py
    run against 9.7.0.0/v10 deployment
    convert to python3
    enforce formatting with black
    update script to get hostname and api key from environment

--- code-examples/authorization-example.py
+++ code-examples/authorization-example.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Demonstrate different results depending on the user
 
 The user of the API is identified by the token that is used, so with SP
@@ -7,11 +7,9 @@ different data.  This example produces a simple report of alert types and
 related managed objects for two diferent user, demonstrating the different
 results for each user"""
 
-from __future__ import print_function
 import os
 import sys
 import requests
-
 
 
 def get_some_alerts(leader, token, cert):
@@ -20,18 +18,19 @@ def get_some_alerts(leader, token, cert):
     For the purposes of this demonstration, we will just get one page
     of alerts and return them as json"""
 
-    url = 'https://{}/api/sp/alerts/?perPage=15'.format(leader)
+    url = "https://{}/api/sp/alerts/?perPage=15".format(leader)
 
-    r = requests.get(url,
-                     headers={
-                         "X-Arbux-APIToken": token,
-                         "Content-Type": 'application/vnd.json+api'
-                         },
-                     verify=cert)
+    r = requests.get(
+        url,
+        headers={"X-Arbux-APIToken": token, "Content-Type": "application/vnd.json+api"},
+        verify=cert,
+    )
 
     if r.status_code is not requests.codes.ok:
-        print("API request for alerts returned {} ({})".format(
-            r.reason, r.status_code), file=sys.stderr)
+        print(
+            "API request for alerts returned {} ({})".format(r.reason, r.status_code),
+            file=sys.stderr,
+        )
         return None
 
     return r.json()
@@ -44,18 +43,21 @@ def alert_report(alerts):
     """
 
     print_nl = False
-    for alert in alerts['data']:
-        print ("{}".format(alert['id']), end="")
-        if 'alert_type' in alert['attributes']:
-            print ("\t{}".format(
-                alert['attributes']['alert_type']), end="")
+    for alert in alerts["data"]:
+        print("{}".format(alert["id"]), end="")
+        if "alert_type" in alert["attributes"]:
+            print("\t{}".format(alert["attributes"]["alert_type"]), end="")
         else:
             print_nl = True
-        if ('subobject' in alert['attributes'] and
-                'misuse_types' in alert['attributes']['subobject']):
-            print ("\t{}".format(
-                ", ".join(alert['attributes']['subobject']['misuse_types'])
-                ))
+        if (
+            "subobject" in alert["attributes"]
+            and "misuse_types" in alert["attributes"]["subobject"]
+        ):
+            print(
+                "\t{}".format(
+                    ", ".join(alert["attributes"]["subobject"]["misuse_types"])
+                )
+            )
         else:
             print_nl = True
 
@@ -65,16 +67,30 @@ def alert_report(alerts):
     return None
 
 
-if __name__ == '__main__':
-    SP_LEADER = os.getenv('SPLEADER')
-    API_TOKEN = {}
-    API_TOKEN['admin'] = os.getenv('ADMIN_TOKEN')
-    API_TOKEN['mssp_user'] = os.getenv('MSSP_TOKEN')
-    CERT_FILE = './certfile'
-    for token in API_TOKEN:
-        print ("Getting alerts for the {} account".format(token))
-        alerts = get_some_alerts(SP_LEADER,
-                                 API_TOKEN[token],
-                                 CERT_FILE)
+if __name__ == "__main__":
+    #
+    # set the SP leader hostname and API key
+    #
+    if "SP_LEADER" in os.environ:
+        SP_LEADER = os.environ["SP_LEADER"]
+    else:
+        exit('no environment variable "SP_LEADER" found')
+
+    # this is assumed to be an admin token
+    if "SP_API_KEY" in os.environ:
+        API_KEY = os.environ["SP_API_KEY"]
+    else:
+        exit('no environment variable "SP_API_KEY" found')
+
+    # this is assumed to be an admin token
+    if "MSSP_TOKEN" in os.environ:
+        MSSP_TOKEN = os.environ["MSSP_TOKEN"]
+    else:
+        exit('no environment variable "MSSP_TOKEN" found')
+    MSSP_TOKEN = os.getenv("MSSP_TOKEN")
+    CERT_FILE = "./certfile"
+    for token in (API_KEY, MSSP_TOKEN):
+        print("Getting alerts for the {} account".format(token))
+        alerts = get_some_alerts(SP_LEADER, token, CERT_FILE)
         if alerts is not None:
             alert_report(alerts)
